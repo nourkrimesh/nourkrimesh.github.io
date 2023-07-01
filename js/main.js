c1cbce04f3435e6fa5fa433ea8637fe514b04208
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-01 18:09:04 +0300

    updated launching time

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
      * ------------------------------------------------------ */
     var ssFinalCountdown = function () {
 
-        var finalDate = new Date("August 15, 2023 15:00:00").getTime();
+        var finalDate = new Date("Aug 15, 2023 15:00:00").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)

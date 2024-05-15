bbf5864fc378effe5cdc3883bf99af3c161b2aeb
Nour Krimesh <nour.krimesh@gmail.com>
2024-05-16 00:58:57 +0400

    update date

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
      * ------------------------------------------------------ */
     var ssFinalCountdown = function () {
 
-        var finalDate = new Date("Jan 25, 2024 15:00:00").getTime();
+        var finalDate = new Date("July 15, 2024 15:00:00").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)
